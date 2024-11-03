2b0290c868640f8292da27ff28a73a83cb3397dc
yvonne1021 <yvonne993919@gmail.com>
2024-11-03 17:23:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 $(document).ready(function() {
-    // Display details on menu item click
-    $('.menu-item').click(function() {
-        const title = $(this).text();
-        const description = $(this).data('description');
-        const price = $(this).data('price');
-        
-        $('#detailTitle').text(title);
-        $('#detailDescription').text(description);
-        $('#detailPrice').text(price);
-        
-        $('#detailView').show();
-    });
-
-    // Reservation form submission
-    $('#reservationForm').submit(function(event) {
-        event.preventDefault();
-        alert('訂位已提交，感謝您的預訂！');
-        this.reset();
+    $(".menu-item").click(function() {
+        var details = $(this).data("details");
+        $("#item-details").text(details);
     });
 });
-
