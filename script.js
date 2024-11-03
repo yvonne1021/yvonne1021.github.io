3eaa084e9d9323e633888ff60c045c83d0b9d812
yvonne1021 <yvonne993919@gmail.com>
2024-11-03 13:11:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
-    $(".menu-item").on("click", function() {
-        var details = $(this).data("details");
-        $("#item-details").fadeOut(200, function() {
-            $(this).text(details).fadeIn(200);
-        });
+    // 顯示菜單項目詳情
+    $(".menu-item").click(function() {
+        const description = $(this).data("description");
+        alert(description);
+    });
+
+    // 提交訂位表單
+    $("#reservation-form").submit(function(event) {
+        event.preventDefault(); // 防止表單提交後刷新頁面
+        const name = $("#name").val();
+        const phone = $("#phone").val();
+        const email = $("#email").val();
+        const people = $("#people").val();
+        const time = $("#time").val();
+
+        $("#reservation-message").html(`
+            <p>感謝您的訂位，${name}！</p>
+            <p>您已成功預約 ${people} 位，時間為 ${time}。</p>
+            <p>我們將會聯絡您，電話: ${phone}，信箱: ${email}。</p>
+        `);
+
+        // 清空表單
+        $("#reservation-form")[0].reset();
     });
 });
